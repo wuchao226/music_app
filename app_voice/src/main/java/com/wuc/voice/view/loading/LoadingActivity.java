5289b92a895d07b7c313c2b858ff70b78fc8da4b
wuchao226 <wuchao226@163.com>
2019-10-18 11:35:51 +0800

    完成基于JobService的保活组件开发

---
+++ app_voice/src/main/java/com/wuc/voice/view/loading/LoadingActivity.java
@@ -0,0 +1,81 @@
+package com.wuc.voice.view.loading;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+
+import com.wuc.lib_common_ui.base.BaseActivity;
+import com.wuc.lib_common_ui.base.constant.Constant;
+import com.wuc.lib_pullalive.AliveJobService;
+import com.wuc.voice.R;
+import com.wuc.voice.view.home.HomeActivity;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+/**
+ * @author: wuchao
+ * @date: 2019-10-18 11:00
+ * @desciption:
+ */
+public class LoadingActivity extends BaseActivity {
+
+    private Handler mHandler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            startActivity(new Intent(LoadingActivity.this, HomeActivity.class));
+            finish();
+        }
+    };
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        avoidLauncherAgain();
+        setContentView(R.layout.activity_loading);
+        pullAliveService();
+        if (hasPermission(Constant.WRITE_READ_EXTERNAL_PERMISSION)) {
+            doSDCardPermission();
+        } else {
+            requestPermission(Constant.WRITE_READ_EXTERNAL_CODE, Constant.WRITE_READ_EXTERNAL_PERMISSION);
+        }
+    }
+
+    /**
+     * 避免从桌面启动程序后，会重新实例化入口类的activity
+     */
+    private void avoidLauncherAgain() {
+        // 判断当前activity是不是所在任务栈的根
+        if (!this.isTaskRoot()) {
+            Intent intent = getIntent();
+            if (intent != null) {
+                String action = intent.getAction();
+                if (intent.hasCategory(Intent.CATEGORY_LAUNCHER) && Intent.ACTION_MAIN.equals(action)) {
+                    finish();
+                }
+            }
+        }
+    }
+
+    private void pullAliveService() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            AliveJobService.start(this);
+        }
+    }
+
+    @Override
+    public void doSDCardPermission() {
+        super.doSDCardPermission();
+        mHandler.sendEmptyMessageDelayed(0, 3000);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mHandler.removeCallbacksAndMessages(null);
+    }
+}
--- lib_common_ui/src/main/java/com/wuc/lib_common_ui/base/BaseActivity.java
+++ lib_common_ui/src/main/java/com/wuc/lib_common_ui/base/BaseActivity.java
@@ -1,11 +1,16 @@
 package com.wuc.lib_common_ui.base;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import com.wuc.lib_common_ui.base.constant.Constant;
 import com.wuc.lib_common_ui.utils.StatusBarUtil;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 /**
  * @author: wuchao
@@ -18,5 +23,57 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         StatusBarUtil.statusBarLightMode(this);
+    }
+
+    /**
+     * 申请指定权限
+     */
+    public void requestPermission(int code, String... permissions) {
+        ActivityCompat.requestPermissions(this, permissions, code);
+    }
+
+    /**
+     * 判断是否有指定的权限
+     */
+    public boolean hasPermission(String... permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case Constant
+                    .WRITE_READ_EXTERNAL_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    doSDCardPermission();
+                }
+                break;
+            case Constant
+                    .HARDWEAR_CAMERA_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    doCameraPermission();
+                }
+                break;
+            default:
+        }
+    }
+
+    /**
+     * 处理整个业务中SDCard业务
+     */
+    public void doSDCardPermission() {
+
+    }
+
+    /**
+     * 处理整个业务中相机业务
+     */
+    public void doCameraPermission() {
+
     }
 }
